93162d893f22999544ec32430515859d33092946
GP <ntvl_steph_983@hotmail.com>
2012-07-03 09:32:51 +0400

    debut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetTest::Application.config.secret_token = '89c6be0a6729ece918be5badfc741ce783145a68ae1c558988a560dd6f6aef0e33d184594c474225dddf4071e64513e70e74387ad9869e1a37d010e046a0765b'
